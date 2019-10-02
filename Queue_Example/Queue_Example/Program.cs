88daeb9359779c5aa324c75d688f3d3680cf48c9
Steven Scott <stevescott2653@gmail.com>
2019-10-02 18:45:24 -0400

    Add files via upload

---
+++ Queue_Example/Queue_Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Queue_Example
+{
+    public class QueueExample
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> names = new Queue<string>();
+            names.Enqueue("Bob");
+            names.Enqueue("John");
+            names.Enqueue("Adam");
+            names.Enqueue("Jim");
+            names.Enqueue("Danny");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Peek element: " +names.Peek());
+            Console.WriteLine("Dequeue: " + names.Dequeue());
+            Console.WriteLine("After Dequeue, Peek element: " + names.Peek());
+        }
+    }
+}
